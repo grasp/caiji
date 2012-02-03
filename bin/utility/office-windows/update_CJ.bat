97c52c76055fdad0fcc4e7d3771af933a2cc1636
grasp <hunter.wxhu@gmail.com>
2012-02-03 10:22:59 +0800

    add office shotcut

--- bin/utility/office-windows/update_CJ.bat
+++ bin/utility/office-windows/update_CJ.bat
@@ -6,6 +6,7 @@ cd %date:~0,10%
 xcopy /s /y d:\vob\caiji
 cd d:\vob
 rm -rf caiji
+set http_proxy=wwwgate0-ch.mot.com:1080
 git clone http://grasp:improvew090#@github.com/grasp/caiji.git
 pause
 
--- bin/utility/office-windows/update_g00.bat
+++ bin/utility/office-windows/update_g00.bat
@@ -6,6 +6,7 @@ cd %date:~0,10%
 xcopy /s /y d:\vob\g00
 cd d:\vob
 rm -rf g00
+set http_proxy=wwwgate0-ch.mot.com:1080
 git clone http://grasp:improvew090#@github.com/grasp/g00.git
 pause
 
