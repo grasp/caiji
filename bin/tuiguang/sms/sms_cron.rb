618dfe1917eca709f754eb931b246d66f604e231
hunter <hunter.wxhu@gmail.com>
2012-02-12 17:12:07 +0800

    change sms sent to -1 as all port

--- bin/tuiguang/sms/sms_cron.rb
+++ bin/tuiguang/sms/sms_cron.rb
@@ -4,43 +4,42 @@
 require 'pathname'
 require 'mongoid'
 require "logger"
-require 'Win32Api'  
+#require 'Win32Api'  
 
 pn = Pathname.new(File.dirname(__FILE__))
-$project_root=pn.parent.parent #do we have one line solution?
+$project_root=pn.parent.parent.parent #do we have one line solution?
 require File.join($project_root,"app","models","mphone.rb")
 require File.join($project_root,"app","helpers","cities_helper.rb")
 
 
-$mongo=Mongo::Connection.new('localhost', 27017)
+#$mongo=Mongo::Connection.new('localhost', 27017)
+$mongo=Mongo::Connection.new('192.168.2.4', 27017)
 Mongoid.database = $mongo.db('caiji_development') 
 
 
 
-MPhone.where(:scount=>nil,:operator=>1).each do |phone|
+Mphone.where(:scount=>nil,:operator=>1).each do |phone|
+ daily_count=Mphone.where(:valid.ne=>false,:scount=>1,:updated_at.gte=>Time.now.at_beginning_of_day).count
+puts "daylycount=#{daily_count}"
+    break if daily_count>990
 begin 
- if LibPhone.where(:sent_counter=>1).count>3200
- break;
- end
-  puts " sending to #{phone.mphone} ,count1=#{MPhone.where(:scount=>1).count}, count0=#{MPhone.where(:scount=>nil).count}"
-  
+  puts " sending to #{phone.mphone} ,count1=#{Mphone.where(:scount=>1).count}, count0=#{Mphone.where(:scount=>nil).count}"  
 	  
-	 advertise="物流零距离网-http://w090.com免费提供大量的货源和车源信息以及货源信息订阅,为您的物流助上一臂之力,欢迎您的访问!"
+	 advertise="物流零距离网-http://w090.com免费提供大量的货源和车源信息以及货源信息订阅,为您的物流事业助上一臂之力,欢迎您的访问!"
 	
 	 #  command="sms.bat"+" "+"15967126712"+" "+advertise
-	 command="sms.bat"+" "+"15394238771"+" "+advertise
+	 command="sms.bat"+" "+"15967126712"+" "+advertise
+	 
+	# puts command
 	# command="sms.bat"+" "+phone.mphone+" "+advertise
 	  sendsms_command = Thread.new do
       system(command) # long-long programm
        sendsms_command.join  		  
-       phone.inc(:scount,1)	 
+      # phone.inc(:scount,1)	 
+ 	 sleep 25
+     end
+  	 rescue
+     puts "Exception in send sms"
+      puts $@
     end
-	 sleep 25
-  end
-
-rescue
-puts "Exception in send sms"
-puts $@
 end
-end
-
